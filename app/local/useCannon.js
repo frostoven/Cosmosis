07b6a2edf3c3ae9489bcdbc190e2e557ba7b66d4
aggregate1166877 <aggregate1166877>
2020-09-27 11:43:05 +1300

    Add visual collision bounds debugging.

--- app/local/useCannon.js
+++ app/local/useCannon.js
@@ -1,22 +1,33 @@
 // https://codesandbox.io/embed/r3f-cannon-physics-nr84m
+// Modified to include a visual collision debugger.
 
-import * as CANNON from 'cannon'
-import React, { useState, useEffect, useContext, useRef } from 'react'
-import { useFrame } from 'react-three-fiber'
+import * as CANNON from 'cannon';
+import * as THREE from 'three';
+import './cannonDebugRenderer';
+import React, { useState, useEffect, useContext, useRef } from 'react';
+import { useFrame, useThree } from 'react-three-fiber';
 
 // Cannon-world context provider
 const context = React.createContext()
 export function Provider({ children }) {
   // Set up physics
   const [world] = useState(() => new CANNON.World())
+
+  // Set up debugger instance
+  const { scene } = useThree();
+  const cannonDebugRenderer = new THREE.CannonDebugRenderer(scene, world);
+
   useEffect(() => {
-    world.broadphase = new CANNON.NaiveBroadphase()
-    world.solver.iterations = 10
+    world.broadphase = new CANNON.NaiveBroadphase();
+    world.solver.iterations = 10;
     // world.gravity.set(0, 0, -25)
   }, [world])
 
   // Run world stepper every frame
-  useFrame(() => world.step(1 / 60))
+  useFrame(() => {
+    world.step(1 / 60);
+    cannonDebugRenderer.update();
+  })
   // Distribute world via context
   return <context.Provider value={world} children={children} />
 }
