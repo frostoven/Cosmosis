835f0792a9f165e31a60104413634ed4ca56f154
aggregate1166877 <aggregate1166877>
2020-10-03 22:08:47 +1300

    Improvements to controls, add control context to PlayerShip.

--- app/local/controls.js
+++ app/local/controls.js
@@ -1,28 +1,41 @@
+// Non-unique key [ctrl/alt/shift/super], on the left side of the keyboard.
 const leftInc = 1000;
+// Non-unique key [ctrl/alt/shift/super], on the right side of the keyboard.
 const rightInc = 2000;
-const numpadInc = 3000;
+// Key belongs to the numpad.
+const numpadInc = 5000;
+// Used to differentiate mouse buttons.
+const mouseInc = 9000;
 
-const keyMap = {
+const keymap = {
   // Arrows
   up: 38,
   left: 37,
   right: 39,
   down: 40,
 
+  // Mouse (using: which)
+  leftClick: 1 + mouseInc,
+  middleClick: 2 + mouseInc,
+  rightClick: 3 + mouseInc,
+  mouse4: 4 + mouseInc,
+  mouse5: 5 + mouseInc,
+  // ^^ this continues below at assignMouseExtra.
+
   // Misc
   space: 32,
 
-  // Modifiers - these are overridden by core with fake values for simplicity
-  // sake.
+  // Modifiers - these are overridden by core.js with fake values for
+  // simplicity sake.
   shift: 16,
-  shift_left: 16 + leftInc,
-  shift_right: 16 + rightInc,
+  shiftLeft: 16 + leftInc,
+  shiftRight: 16 + rightInc,
   ctrl: 17,
-  ctrl_left: 17 + leftInc,
-  ctrl_right: 17 + rightInc,
+  ctrlLeft: 17 + leftInc,
+  ctrlRight: 17 + rightInc,
   alt: 18,
-  alt_left: 18 + leftInc,
-  alt_right: 18 + rightInc,
+  altLeft: 18 + leftInc,
+  altRight: 18 + rightInc,
 
   // Alphabet
   a: 65,
@@ -33,29 +46,122 @@ const keyMap = {
   w: 87,
 };
 
+/**
+ * There are some insane pieces of mouse tech out there that go a bit too far
+ * and curse us with their existence, such as this 20 button abomination:
+ * https://www.amazon.com/Logitech-Gaming-Backlit-Programmable-Buttons/dp/B0086UK7IQ#:~:text=For%20complete%20control%20in%20your,for%20mastering%20your%20favorite%20MMOs.
+ * @param min
+ * @param max
+ */
+function assignMouseExtra(min, max) {
+  for (; min <= max; min++) {
+    keymap[`mouse${min}`] = min + mouseInc;
+  }
+}
+assignMouseExtra(6, 20);
+
 const controls = {
   allModes: {
-    lockMouse: [ keyMap.ctrl_left ],
+    lockMouse: [ keymap.ctrlLeft ],
   },
   shipPilot: {
-    thrustInc: [ keyMap.w ],
-    thrustDec: [ keyMap.s ],
-    back_renameme: [ keyMap.a ],
-    right_renameme: [ keyMap.d ],
+    thrustInc: [ keymap.w ],
+    thrustDec: [ keymap.s ],
+    thrustReset: [ keymap.middleClick ],
+    left_renameme: [ keymap.a ],
+    right_renameme: [ keymap.d ],
   },
   freeCam: {
     _description: 'Free camera',
-    forward: [ keyMap.w, keyMap.up ],
-    back: [ keyMap.s, keyMap.down ],
-    left: [ keyMap.a, keyMap.left ],
-    right: [ keyMap.d, keyMap.right ],
-    up: [ keyMap.r, keyMap.space ],
-    down: [ keyMap.f ],
+    forward: [ keymap.w, keymap.up ],
+    back: [ keymap.s, keymap.down ],
+    left: [ keymap.a, keymap.left ],
+    right: [ keymap.d, keymap.right ],
+    up: [ keymap.r, keymap.space ],
+    down: [ keymap.f ],
   }
 }
 
 /**
- * Takes user input, and check if it's defined in keyMap above. If not, ignores
+ * Used to convert literal names like 'Right' to something a bit more
+ * meaningful like 'Right Arrow'.
+ */
+const friendlierKeyName = {
+  'Up': 'Up Arrow',
+  'Left': 'Left Arrow',
+  'Right': 'Right Arrow',
+  'Down': 'Down Arrow',
+  //
+  'Shift Left': 'Left Shift',
+  'Shift Right': 'Right Shift',
+  'Ctrl Left': 'Left Ctrl',
+  'Ctrl Right': 'Right Ctrl',
+  'Alt Left': 'Left Alt',
+  'Alt Right': 'Right Alt',
+  //
+  'Mouse20': 'Mega mouse click',
+};
+
+/**
+ * Converts a keymap name to a friendlier name that can be displayed to the
+ * user.
+ * Note: this function is slow; please don't run it every frame.
+ * @param {number|string} key - The key you want a name for. This can be text,
+ *  or the actual numeric key as defined in keymap.
+ */
+function keymapFriendlyName(key) {
+  if (typeof key === 'number') {
+    // Start be converting the number to the string equivalent.
+    const mapKeys = Object.keys(keymap);
+    for (let i = 0, len = mapKeys.length; i < len; i++) {
+      const str = mapKeys[i];
+      if (key === keymap[str]) {
+        key = str;
+        break;
+      }
+    }
+  }
+
+  if (typeof key === 'number') {
+    // Nothing was matched above.
+    return `Key ${key}`;
+  }
+
+  let result = key.replace( /([A-Z])/g, " $1" );
+  result = result.charAt(0).toUpperCase() + result.slice(1);
+  const friendlier = friendlierKeyName[result];
+  if (friendlier) {
+    return friendlier;
+  }
+  else {
+    return result;
+  }
+
+  // let currentWord = '';
+  // const words = [];
+  // for (let i of key) {
+  //   if (!i.match(/^[a-z]+$/)) {
+  //     words.push(currentWord);
+  //     currentWord = '';
+  //   }
+  //   currentWord += i.toLocaleLowerCase();
+  // }
+  //
+  // if (currentWord) {
+  //   words.push(currentWord);
+  // }
+  //
+  // // Capitilise first word.
+  // if (words.length > 0) {
+  //   words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
+  // }
+  //
+  // return words.join(' ');
+}
+// window.keymapFriendlyName = keymapFriendlyName;
+
+/**
+ * Takes user input, and check if it's defined in keymap above. If not, ignores
  * keyPress. Else, calls back with the new key and terminates. This function is
  * specifically for letting the user choose key bindings in the controls menu.
  * @param ignoreKeyLocation
@@ -66,6 +172,6 @@ function detectKeyPress({ ignoreKeyLocation=false }={}, onPress) {
 }
 
 module.exports = {
-  keyMap,
+  keymap,
   controls,
 };
--- app/models/PlayerShip.js
+++ app/models/PlayerShip.js
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import { useFrame, useThree } from 'react-three-fiber';
 
 import core from '../local/core';
 import { controls } from '../local/controls';
@@ -10,15 +11,44 @@ const {
   registerKeyPress, deregisterKeyPress, registerKeyUpDown, deregisterKeyUpDown,
 } = core;
 
+/**
+ * Unsure how to use r3f hooks in normal components, so just wrapping for now.
+ * @param {object} props
+ * @returns {number[] | SamplingHeapProfileNode[] | NodeJS.Module[] | HTMLCollection}
+ * @constructor
+ */
+function PlayerController(props) {
+  props.useThree(useThree());
+  useFrame(props.useFrame);
+  return props.children;
+}
+
 export default class PlayerShip extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    // If true, does initial setup on first animation frame.
+    this.doInit = true;
+    // The ship we're currently in.
+    this.shipModel = null;
+    // The position our camera is currently tracking.
+    this.camTarget = null;
+    // The camera our screen is following.
+    this.sceneCam = null;
+
+    this.thrust = 0;
+    this.thrustDelta = 1;
+    this.incThrust = false;
+    this.decThrust = false;
+    this.resetThrust = false;
   }
 
-  onUpdate = ({ model }) => {
-    const cam1 = model.nodes.BridgeCam1;
-  }
+  onShipUpdate = ({ model }) => {
+    this.shipModel = model;
+    // this.camTarget = model.nodes.BridgeCam1;
+    this.camTarget = model.nodes.maintenance_cover;
+  };
 
   componentDidMount() {
     registerModeListener(this.onModeChange);
@@ -30,14 +60,24 @@ export default class PlayerShip extends React.Component {
     deregisterKeyUpDown({ mode: modes.shipPilot, cb: this.onKeyUpDown });
   }
 
+  setContexts = (contexts) => {
+    this.sceneCam = contexts.camera;
+  };
+
   onKeyUpDown = ({ key, isDown }) => {
     // console.log(`Player ship: key=${key}, isDown=${isDown}`);
     const ctrl = controls.shipPilot;
     if (ctrl.thrustInc.includes(key)) {
-      console.log('Player ship: increase thrust.');
+      // console.log('Player ship: increase thrust.');
+      this.incThrust = isDown;
     }
     else if (ctrl.thrustDec.includes(key)) {
-      console.log('Player ship: decrease thrust.');
+      // console.log('Player ship: decrease thrust.');
+      this.decThrust = isDown;
+    }
+    else if (ctrl.thrustReset.includes(key)) {
+      // Always reset
+      this.resetThrust = true;
     }
   };
 
@@ -45,13 +85,66 @@ export default class PlayerShip extends React.Component {
     console.log(`Detected mode change: mode=${mode}, prevMode=${prevMode}`);
   }
 
+  animate = (state, deltaTime) => {
+    if (this.doInit) {
+      if (this.camTarget) {
+        this.doInit = false;
+        // this.camTarget.attach(this.sceneCam);
+        console.log('scene cam:', this.sceneCam);
+        setTimeout(() => {
+          console.log('cam target:', this.camTarget);
+        }, 2000);
+      }
+    }
+    this.lockCameraTo(this.camTarget);
+
+    if (this.incThrust) {
+      this.thrust += this.thrustDelta * deltaTime;
+      // console.log('Thrust is:', this.thrust);
+    }
+    if (this.decThrust) {
+      this.thrust -= this.thrustDelta * deltaTime;
+      // console.log('Thrust is:', this.thrust);
+    }
+    if (this.resetThrust) {
+      this.resetThrust = false;
+      this.thrust = 0;
+      // console.log('Thrust is:', this.thrust);
+    }
+  };
+
+  lockCameraTo = (cam) => {
+    if (this.camTarget && this.sceneCam) {
+      this.sceneCam.position.copy(this.camTarget.position);
+
+      // this.sceneCam.updateProjectionMatrix();
+      // this.sceneCam.position.x = this.camTarget.position.x;
+      // this.sceneCam.position.y = this.camTarget.position.y;
+      // this.sceneCam.position.z = this.camTarget.position.z;
+    }
+  };
+
+  // increaseThrust = (deltaTime) => {
+  //   this.thrust += this.thrustDelta;
+  // };
+  //
+  // decreaseThrust = (deltaTime) => {
+  //   this.thrust -= this.thrustDelta;
+  // };
+
+  resetThrust = () => {
+    this.thrust = 0;
+  };
+
   render() {
     return (
-      <Ship
-        {...this.props}
-        model="DS69F"
-        onUpdate={this.onUpdate}
-      />
+      <PlayerController useThree={this.setContexts} useFrame={this.animate}>
+        <Ship
+          {...this.props}
+          model="DS69F"
+          onUpdate={this.onShipUpdate}
+        />
+      </PlayerController>
     )
   }
 }
