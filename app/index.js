28b29218165c6b47490a002521955e74e50da782
aggregate1166877 <aggregate1166877>
2021-05-31 18:10:28 +1200

    Fix bug where application reloading no longer works on latest NW.js.

--- app/index.js
+++ app/index.js
@@ -48,7 +48,9 @@ if (process.env && process.env.NODE_ENV !== 'production') {
       setTimeout(() => {
         // Webpack sometimes modifies files multiple times in a short span,
         // causing a broken reload. Wait a bit for it to finish.
-        nw.Window.get().reload();
+        // Currently a bug in nw.js. TODO: remove once they fix it.
+        // nw.Window.get().reload();
+        chrome.tabs.reload()
       }, 250);
     }
   });
