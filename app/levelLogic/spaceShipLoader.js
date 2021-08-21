04d89b8c47de8b76d41fdd603e65c705399bb317
aggregate1166877 <aggregate1166877>
2021-08-21 15:46:19 +1200

    #7 Move lighting functions into their own files / boot processes.

--- app/levelLogic/spaceShipLoader.js
+++ app/levelLogic/spaceShipLoader.js
@@ -133,17 +133,9 @@ function modelPostSetup(modelName, gltf, pos, scene, world, onReady) {
     matrix.makeRotationFromQuaternion(quaternion);
     mesh.scene.applyMatrix4(matrix);
 
-    // TODO: remove this light. This is only here until lights become dynamic.
-    // const warmWhite = 0xefebd8;
     // const warmWhite = 0xfff5b6;
-    const warmWhite = 0xfff5b6;
-    // const light = new THREE.PointLight( warmWhite, 1.5, 100 );
 
-    // const light = new THREE.PointLight( warmWhite, 2, 100 );
-    // const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-    // light.castShadow = true;
-
-    // Make the ship cast a shadow:
+    // Make the ship cast/receive shadows, including self-shadows:
     gltf.scene.traverse(function(node) {
       if (node.isMesh) {
         // Backface culling. Without this shadows get somewhat insane because
@@ -155,90 +147,6 @@ function modelPostSetup(modelName, gltf, pos, scene, world, onReady) {
       }
     });
 
-    // const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
-    // hemiLight.position.set( 0, 200, 0 );
-    // bubble.add(hemiLight);
-
-    // const light = new THREE.DirectionalLight(0xfff5b6, 2.5);
-    const light = new THREE.DirectionalLight(0xffffff, 2);
-    light.castShadow = true;
-
-    // TODO: move into graphics menu as 'horizontal shadow distance (in meters)'.
-    const shadowCamWidth = 3;
-    const shadowCamHeight = 3;
-
-    light.shadow.camera.top = shadowCamHeight;
-    light.shadow.camera.bottom = -shadowCamHeight;
-    light.shadow.camera.left = -shadowCamWidth;
-    light.shadow.camera.right = shadowCamWidth;
-    light.shadow.camera.near = 0.5;
-    light.shadow.camera.far = 5;
-    // ^^ ?
-    // TODO: move to graphics as 'vertical shadow distance (meters)'
-    // const shadowDistanceVertically = 1.5;
-
-    light.position.set(0, 3.5, 0);
-    // Note: target should *not* be used to adjust distance. Use camera.far for
-    // that.
-    light.target.position.set(0, 1, 0);
-
-    // light.shadow.camera = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
-
-    // TODO: move into graphics are 'shadow resolution'.
-    light.shadow.mapSize.width = 1024 * 4;
-    light.shadow.mapSize.height= 1024 * 4;
-
-    const lightHelper = new THREE.DirectionalLightHelper(light, 5);
-    bubble.add(lightHelper);
-
-    const shadowHelper = new THREE.CameraHelper(light.shadow.camera);
-    bubble.add(shadowHelper);
-
-    light.target.updateMatrixWorld();
-
-    bubble.add(light);
-    window.light = light;
-    // light.target.updateMatrixWorld();
-
-
-    // // ground
-    // const floor = new THREE.Mesh(new THREE.PlaneGeometry(15, 15), new THREE.MeshPhongMaterial({ color: 0x999999 }));
-    // floor.rotation.x = - Math.PI / 2;
-    // floor.receiveShadow = true;
-    // bubble.add( floor );
-    //
-    // const grid = new THREE.GridHelper(15, 20, 0x000000, 0x000000);
-    // grid.material.opacity = 0.2;
-    // grid.material.transparent = true;
-    // bubble.add(grid);
-
-
-
-
-    // light.position.set(pos.x, pos.y + 2, pos.z);
-    // light.position.set(0, 2, 0);
-
-    // light.position.set(0, 2, -5);
-    // mesh.scene.add(light);
-
-    //
-    const light2 = new THREE.PointLight( warmWhite, 2, 100 );
-
-
-    // light.position.set(pos.x, pos.y + 2, pos.z);
-    light2.position.set(0, 2, 3);
-    // mesh.scene.add(light2);
-
-    // TODO: implement the physics things.
-    // const body = makePhysical({
-    //   mesh: mesh.scene,
-    //   // TODO: for now we use a simple cube shape just to get the framework up.
-    //   //  This needs to become a compound shape in future using the p[a]_ tags
-    //   //  (see mesh codes -> Physics collision node codes).
-    //   bodyShape: shapeTemplates.cubeShape(1.2, 1, 1.8),
-    //   options: { mass: 1 },
-    //   world,
-    // });
     onReady(mesh, bubble);
   });
 }
