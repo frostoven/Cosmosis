3a327aa1335250c037a80bb319c37c160f11b222
aggregate1166877 <aggregate1166877>
2021-08-21 15:47:27 +1200

    #7 Adjust planet and star mesh materials.

--- app/scenes/localCluster.js
+++ app/scenes/localCluster.js
@@ -5,32 +5,48 @@ import * as THREE from 'three';
 
 import core from '../local/core';
 
-// const demo = new CANNON.Demo();
-// console.log(demo)
-
 var quality = 16, step = 1024 / quality;
 
+// Note on label data - if for whatever insane reason we'd ever want to go
+// below 1cm size, use scale instead.
 const labelData = [
-  // {size: .01, scale: 0.0001, label: "microscopic (1µm)"}, // FIXME - triangulating text fails at this size, so we scale instead
-  // {size: .01, scale: 0.1, label: "minuscule (1mm)"},
-  // {size: .01, scale: 1.0, label: "tiny (1cm)"},
-  // {size: 1, scale: 1.0, label: "child (1m)"},
-  // {size: 10, scale: 1.0, label: "tree (10m)"},
-  // {size: 100, scale: 1.0, label: "building (100m)"},
-  // {size: 1000, scale: 1.0, label: "medium (1km)"},
-  // {size: 10000, scale: 1.0, label: "city (10km)"},
-  // https://www.scientificamerican.com/article/why-do-the-moon-and-the-s/#:~:text=Because%20the%20moon%20is%20changing,the%20moon%20appear%20very%20large.
-  // TODO: make these use the res loader.
-  {size: 3474000, scale: 1.0, label: "moon (3,474 Km)", grouped: false, nogroupOffset: -384400000, image: 'prodHqAssets/planetImg/Moon_lroc_color_poles_8k.jpg'},
-  {size: 12742000, scale: 1.0, label: "earth (12,742 km)", grouped: false, nogroupOffset: 0, image: 'prodHqAssets/planetImg/Land_ocean_ice_cloud_hires.jpg'},
-  {size: 1392700000, scale: 1.0, label: "sun (1,392,700 km)", brightness: 10, grouped: false, nogroupOffset: 149540000000, image: 'prodHqAssets/planetImg/sun_euvi_aia304_2012_carrington.jpg'},
-  // {size: 3474000, scale: 1.0, label: "moon (3,474 Km)", grouped: false, nogroupOffset: 384400000, image: 'potatoLqAssets/planetImg/Moon_lroc_color_poles_8k.jpg'},
-  // {size: 12742000, scale: 1.0, label: "earth (12,742 km)", grouped: false, nogroupOffset: 0, image: 'potatoLqAssets/planetImg/Land_ocean_ice_cloud_hires.jpg'},
-  // {size: 1392700000, scale: 1.0, label: "sun (1,392,700 km)", brightness: 10, grouped: false, nogroupOffset: -149540000000, image: 'potatoLqAssets/planetImg/sun_euvi_aia304_2012_carrington.jpg'},
-  {size: 7.47e12, scale: 1.0, label: "solar system (50Au)"},
-  {size: 9.4605284e15, scale: 1.0, label: "gargantuan (1 light year)"},
-  {size: 3.08567758e16, scale: 1.0, label: "ludicrous (1 parsec)"},
-  {size: 1e19, scale: 1.0, label: "mind boggling (1000 light years)"}
+  // TODO: load via planetary system and use the res loader.
+  {
+    name: 'sun',
+    size: 1392700000, // 1,392,700 km
+    reflectivity: 0,
+    scale: 1.0,
+    brightness: 10,
+    grouped: false,
+    nogroupOffset: 149540000000,
+    image: 'prodHqAssets/planetImg/sun_euvi_aia304_2012_carrington.jpg',
+  },
+  {
+    name: 'moon',
+    // https://www.scientificamerican.com/article/why-do-the-moon-and-the-s/#:~:text=Because%20the%20moon%20is%20changing,the%20moon%20appear%20very%20large
+    size: 3474000, // 3,474 km
+    reflectivity: 0.12, // yes, it's actually that high.
+    scale: 1.0,
+    grouped: false,
+    nogroupOffset: -384400000,
+    image: 'prodHqAssets/planetImg/Moon_lroc_color_poles_8k.jpg',
+  },
+  {
+    name: 'earth',
+    size: 12742000, // 12,742 km
+    reflectivity: 0.3, // yes, it's actually that low.
+    scale: 1.0,
+    grouped: false,
+    nogroupOffset: 0,
+    image: 'prodHqAssets/planetImg/Land_ocean_ice_cloud_hires.jpg',
+  },
+
+  // Going to miss these. They really helped me prototype the initial system.
+  // Maybe one day I can offer the original authors my thanks.
+  // {size: 7.47e12, scale: 1.0, label: "solar system (50Au)"},
+  // {size: 9.4605284e15, scale: 1.0, label: "gargantuan (1 light year)"},
+  // {size: 3.08567758e16, scale: 1.0, label: "ludicrous (1 parsec)"},
+  // {size: 1e19, scale: 1.0, label: "mind boggling (1000 light years)"}
 ];
 
 function register() {
@@ -42,17 +58,9 @@ function register() {
 
 function init({ font }) {
   const scene = new THREE.Scene();
-  // scene.add(new THREE.AmbientLight(0x222222));
-  // const light = new THREE.DirectionalLight(0xffffff, 1);
-  // light.position.set(100, 100, 100);
-  // scene.add(light);
 
   // Massive space bodies part below.
   const geometry = new THREE.SphereBufferGeometry(0.5, 24*10, 12*10);
-
-  // var data = generateHeight( 1024, 1024 );
-  // var texture = new THREE.Texture( generateTexture( data, 1024, 1024 ) );
-  // texture.needsUpdate = true;
 
   for (let i = 0; i < labelData.length; i++) {
     const body = labelData[i];
@@ -66,7 +74,7 @@ function init({ font }) {
       emissiveIntensity: body.brightness ? body.brightness : 1,
     };
 
-    const labelGeo = new THREE.TextBufferGeometry(body.label, {
+    const labelGeo = new THREE.TextBufferGeometry(body.name, {
       font: font,
       size: body.size,
       height: body.size / 2
@@ -92,8 +100,6 @@ function init({ font }) {
       group.add(textMesh);
     }
 
-    // console.log(`[z] ${body.label}:`, -body.size * scale);
-
     if (body.image) {
       const loader = new THREE.TextureLoader();
       loader.load( body.image, function ( texture ) {
@@ -110,11 +116,28 @@ function init({ font }) {
         // // plane.computeCentroids();
         // plane.computeFaceNormals();
 
-        const material = new THREE.MeshBasicMaterial({map: texture});
-        // const material = new THREE.MeshPhongMaterial({map: texture});
+        // const material = new THREE.MeshBasicMaterial({map: texture});
+
+        // We'll probably want to do most reflectivity with shaders (because
+        // reflections would be unevenly distributed, ex. ocean vs land).
+        let material;
+        let castShadow = false;
+        let receiveShadow = true;
+
+        if (!body.reflectivity) {
+          // If has no reflectivity, may as well go with something cheaper that
+          // doesn't support reflectivity at all (usually used for stars).
+          material = new THREE.MeshBasicMaterial({ map: texture });
+          receiveShadow = false;
+        }
+        else {
+          material = new THREE.MeshPhongMaterial({ map: texture });
+          material.reflectivity = body.reflectivity;
+        }
+
         const mesh = new THREE.Mesh(geometry, material);
-        // mesh.castShadow = true;
-        // mesh.receiveShadow = true;
+        mesh.castShadow = castShadow;
+        mesh.receiveShadow = receiveShadow;
 
         // const mesh = new THREE.Mesh(plane, material);
         // mesh.position.y = -body.size / 4 * scale;
@@ -152,7 +175,7 @@ function init({ font }) {
 //  not being utilised.
 // startupEmitter.on(startupEvent.ready, () => {
 //   const objects = generateCubeField({
-//     scene: $game.scene,
+//     scene: $game.levelScene,
 //     position: $game.camera.position,
 //   });
 //   console.log('cube space:', objects);
@@ -160,7 +183,7 @@ function init({ font }) {
 
 // startupEmitter.on(startupEvent.ready, () => {
 //   const objects = generateCubeField({
-//     scene: $game.scene,
+//     scene: $game.levelScene,
 //     position: $game.camera.position,
 //     cubeCount: 25,
 //     distanceMultiplier: 0.5
@@ -169,7 +192,7 @@ function init({ font }) {
 
 // startupEmitter.on(startupEvent.ready, () => {
   // const objects = generateCubeField({
-  //   scene: $game.scene,
+  //   scene: $game.levelScene,
   //   position: $game.camera.position,
   //   cubeCount: 100,
   // });
