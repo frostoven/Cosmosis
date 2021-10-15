334e1e1465fc38709f1b54c42eac2ff0701c641a
aggregate1166877 <aggregate1166877>
2021-10-15 19:26:34 +1300

    Add additional debug warp locations.

--- app/userProfile/defaultsConfigs/debugTools.js
+++ app/userProfile/defaultsConfigs/debugTools.js
@@ -5,48 +5,62 @@ const debugTools = {
   },
   fileContent: {
     defaultShipPosition: {
-      text: 'Directly facing the moon (default).',
-      location: { x: -381752594, y: -691327, z: 1417254 },
-      rotation: { x: -2.5974, y: -0.9536, z: -2.5171 },
+      // text: 'Directly facing the moon (default).',
+      // location: { x: -381752594, y: -691327, z: 1417254 },
+      // rotation: { x: -2.5974, y: -0.9536, z: -2.5171 }
+      //
+      text: '🏹 Facing the Orion constellation',
+      location: { x: 0, y: 0, z: 0 },
+      rotation: { x: 1.4699, y: -0.0237, z: -1.5543 },
     },
     storedShipPositions: [
       {
-        text: 'Directly facing the moon (default).',
+        text: '🏹 Facing the Orion constellation',
+        location: { x: 0, y: 0, z: 0 },
+        rotation: { x: 1.4699, y: -0.0237, z: -1.5543 },
+      },
+      {
+        text: '🐕 Facing Canis Manor',
+        location: { x: 0, y: 0, z: 0 },
+        rotation: { x: 1.7728, y: -0.3999, z: -2.8868 },
+      },
+      {
+        text: '☾ Directly facing the moon (default).',
         location: { x: -381752594, y: -691327, z: 1417254 },
         rotation: { x: -2.5974, y: -0.9536, z: -2.5171 },
       },
       {
-        text: 'Next to moon, good place to test lighting.',
+        text: '☾ Next to moon, good place to test lighting.',
         location: { x: -390249080, y: -2468483, z: 5996841 },
-        rotation: { x: 2.5626, y: -1.212, z: 2.6454 },
+        rotation: { x: -3.0703, y: 1.3197, z: -2.9520 },
       },
       {
-        text: 'Facing Earth, moon in view; back to sun.',
+        text: '⊕ Facing Earth, moon in view; back to sun.',
         location: { x: 26914792, y: -5727037, z: 5578466 },
-        rotation: { x: 0.8734, y: 1.537, z: 1.6702 },
+        rotation: { x: 0.6760, y: -1.5172, z: 1.4438 },
       },
       {
-        text: 'Behind the Earth; night (shadow testing).',
+        text: '⊕ Behind the Earth; night (shadow testing).',
         location: { x: -12715908, y: -3376086, z: 3944229 },
         rotation: { x: 2.3031, y: -1.1355, z: 0.7181 },
       },
       {
-        text: 'Close to moon (overexposure testing).',
+        text: '☾ Close to moon (overexposure testing).',
         location: { x: -384046753, y: 938728, z: -2120968 },
         rotation: { x: 0.2875, y: -0.0703, z: 0.0226 },
       },
       {
-        text: 'On Earth\'s surface at night, facing moon.',
+        text: '☾ On Earth\'s surface at night, facing moon.',
         location: { x: 26914792, y: -5727037, z: 5578466 },
         rotation: { x: 0.8734, y: 1.537, z: 1.6702 },
       },
       {
-        text: 'Behind Earth. Useful for testing shadow casting, atmospheric lensing, etc.',
+        text: '⊕ Behind Earth. Useful for testing shadow casting, atmospheric lensing, etc.',
         location: { x: -27656524, y: 835865, z: -766508 },
         rotation: { x: -0.1851, y: -1.5549, z: -0.0682 },
       },
       {
-        text: 'Next to that crazy fucking huge fireball.',
+        text: '☉ Next to that crazy fucking huge fireball.',
         location: { x: 150064242871, y: 485401441, z: -392001660 },
         rotation: { x: -0.8526, y: -0.3184, z: -1.4681 },
       },
