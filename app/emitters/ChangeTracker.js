c354f0e722a0d32259e90a8ebbe8403072c460d0
aggregate1166877 <aggregate1166877>
2021-12-04 21:11:21 +1300

    #55 Add ChangeTracker tests.

--- app/emitters/ChangeTracker.js
+++ app/emitters/ChangeTracker.js
@@ -70,3 +70,55 @@ export default class ChangeTracker {
     }
   }
 }
+
+/* ============================================================== */
+
+function test() {
+  const testTracker = new ChangeTracker();
+
+  testTracker.getOnce((value) => {
+    console.log('[1] getOnce - should get "1":', value);
+    if (value !== 1) throw `[1] ChangeTracker: expected 1, got ${value}`;
+  });
+
+  testTracker.getNext((value) => {
+    console.log('[2] getNext - should get "1":', value);
+    if (value !== 1) throw `[2] ChangeTracker: expected 1, got ${value}`;
+  });
+
+  testTracker.getEveryChange((value) => {
+    console.log('[3] getEveryChange - should get "1, 2, 3":', value);
+    if (![ 1, 2, 3 ].includes(value)) throw `[3] ChangeTracker: expected [1|2|3], got ${value}`;
+  });
+
+  testTracker.setValue(1);
+  setTimeout(() => {
+    testTracker.setValue(2);
+
+    testTracker.getOnce((value) => {
+      console.log('[4] getOnce - should get "2":', value);
+      if (value !== 2) throw `[4] ChangeTracker: expected 2, got ${value}`;
+    });
+  }, 0);
+
+  setTimeout(() => {
+    testTracker.setValue(2);
+  }, 200);
+
+  setTimeout(() => {
+    testTracker.getNext((value) => {
+      console.log('[5] getNext - should get "3":', value);
+      if (value !== 3) throw `[5] ChangeTracker: expected 3, got ${value}`;
+    });
+
+    testTracker.setValue(3);
+  }, 500);
+}
+
+const asyncTests = {
+  testChangeTracker: test,
+};
+
+export {
+  asyncTests,
+}
