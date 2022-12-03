df0e0d267da55e2d93938529fe1cbb2659f75761
aggregate1166877 <aggregate1166877>
2022-12-03 19:51:47 +1300

    #88 Add MetaData plugin.

--- app/plugins/pluginsEnabled.ts
+++ app/plugins/pluginsEnabled.ts
@@ -1,20 +1,13 @@
 import { PluginEntry } from './interfaces/PluginEntry';
+import { metaDataPlugin } from './built-ins/MetaData';
 import { playerPlugin } from './built-ins/Player';
 
 const builtInPluginsEnabled: PluginEntry[] = [
-  {
-    name: 'player',
-    pluginInstance: playerPlugin,
-  }
-  // {
-  //   name: 'levelScene',
-  // },
-  // {
-  //   name: 'spaceScene',
-  // },
-  // {
-  //   name: 'playerShip',
-  // },
+  { name: 'metaData', pluginInstance: metaDataPlugin },
+  { name: 'player', pluginInstance: playerPlugin },
+  // { name: 'levelScene' },
+  // { name: 'spaceScene' },
+  // { name: 'playerShip' },
 ];
 
 export {
