797b155fc05ad0649edb69f20968f08aa6bad072
aggregate1166877 <aggregate1166877>
2022-12-04 16:40:03 +1300

    #88 Initial work on the location plugin managing scenes.

--- app/plugins/built-ins/Location/index.ts
+++ app/plugins/built-ins/Location/index.ts
@@ -1,4 +1,4 @@
-import { PerspectiveCamera, Vector3 } from 'three';
+import { PerspectiveCamera, Scene, Vector3 } from 'three';
 import { gameState } from '../../gameState';
 import CosmosisPlugin from '../../types/CosmosisPlugin';
 import { CoordType } from './types/CoordType';
@@ -9,19 +9,31 @@ class Location {
   // and the scene's own distance units. This variable's units are in meters.
   public effectiveCoords: Vector3;
   // public worldScaleVector: Vector3;
-  private _cachedCamera: PerspectiveCamera;
   public coordMode: CoordType;
+
+  private _cachedCamera: PerspectiveCamera;
+  private _cachedLevelScene: Scene;
+  private _cachedSpaceScene: Scene;
 
   constructor() {
     this.setupWatchers();
     this.effectiveCoords = new Vector3();
-    this._cachedCamera = new PerspectiveCamera();
     this.coordMode = CoordType.playerCentric;
+
+    this._cachedCamera = new PerspectiveCamera();
+    this._cachedLevelScene = new Scene();
+    this._cachedSpaceScene = new Scene();
   }
 
   setupWatchers() {
     gameState.tracked.player.getEveryChange((camera) => {
       this._cachedCamera = camera;
+    });
+    gameState.tracked.levelScene.getEveryChange((scene) => {
+      this._cachedLevelScene = scene;
+    });
+    gameState.tracked.spaceScene.getEveryChange((scene) => {
+      this._cachedSpaceScene = scene;
     });
   }
 
@@ -31,7 +43,7 @@ class Location {
 
   // Moves the player relative to the world, or moves the world relative to the
   // player, depending on current world coordinate mode.
-  update(direction: Vector3, speed) {
+  add(direction: Vector3, speed) {
     // if moving player with physics, just update ship position.
     // if moving universe, just update universe position.
   }
