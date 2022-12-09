90117940833b64b03b15bcb18d700e1d149ffacd
aggregate1166877 <aggregate1166877>
2022-12-10 03:54:18 +1300

    #88 Rethink key action types.

--- app/plugins/built-ins/InputManager/types/ActionType.ts
+++ app/plugins/built-ins/InputManager/types/ActionType.ts
@@ -1,13 +1,21 @@
 enum ActionType {
-  // Indicates that the control action should be boolean: true means the key is
-  // being help down, false means the key is not being held down.
-  binary = 2,
   // This triggers a single callback per key press. Can also be thought of as a
   // toggleable in some instances (e.g. press for roof lights, press again for
   // no lights).
   pulse = 4,
-  // Mouse and game controllers.
-  analog = 8,
+  // Sets things to the exact current mouse or game stick value.
+  // If a keyboard button, the value is either 0% or 100% (0-1).
+  analogLiteral = 8,
+  // Add the latest mouse or game stick value.
+  // If a keyboard button, the value either 0% or 100% (0-1).
+  analogAdditive = 16,
+  // Sets the value to the device's change in momentum.
+  // If a keyboard button, the value either 0% or 100% (0-1).
+  analogGravity = 64,
+
+  // Indicates that the control action should be boolean: true means the key is
+  // being help down, false means the key is not being held down.
+  // binary = 2,
 }
 
 export {
