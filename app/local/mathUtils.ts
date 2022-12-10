403795164c6185e3b23090ac111bd0b5a9cb3953
aggregate1166877 <aggregate1166877>
2022-12-10 18:48:49 +1300

    #88 Add math utils file.

---
+++ app/local/mathUtils.ts
@@ -0,0 +1,17 @@
+import { Euler } from 'three';
+
+const _aprEuler = new Euler(0, 0, 0, 'YXZ');
+function applyPolarRotation(x, y, observerQuaternion, minPolarAngle = 0, maxPolarAngle = Math.PI) {
+  _aprEuler.set(0, 0, 0);
+  const PI_2 = Math.PI / 2;
+
+  _aprEuler.setFromQuaternion(observerQuaternion);
+  _aprEuler.y = x * -0.002;
+  _aprEuler.x = y * -0.002;
+  _aprEuler.x = Math.max(PI_2 - maxPolarAngle, Math.min(PI_2 - minPolarAngle, _aprEuler.x));
+  observerQuaternion.setFromEuler(_aprEuler);
+}
+
+export {
+  applyPolarRotation,
+}
