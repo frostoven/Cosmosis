50030733208884a2f83ad8c8161922acda366e61
aggregate1166877 <aggregate1166877>
2022-12-12 00:02:02 +1300

    #88 Add initial ship module base classes.

---
+++ app/plugins/built-ins/shipModules/types/ModuleSpawner.ts
@@ -0,0 +1,5 @@
+export default class ModuleSpawner {
+  createPart(): any {
+    throw 'Child class has not implemented createPart()';
+  }
+}
---
+++ app/plugins/built-ins/shipModules/types/ShipModule.ts
@@ -0,0 +1,24 @@
+export default class ShipModule {
+  constructor() {
+  }
+
+  step() {
+    //
+  }
+
+  connectPowerIn() {
+    //
+  }
+
+  disconnectPowerIn() {
+    //
+  }
+
+  connectPowerOut() {
+    //
+  }
+
+  disconnectPowerOut() {
+    //
+  }
+}
