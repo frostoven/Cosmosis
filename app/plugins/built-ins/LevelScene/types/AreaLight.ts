2625cde4d6767ac5c2dffbb7a03830799f570170
aggregate1166877 <aggregate1166877>
2022-12-12 02:26:45 +1300

    #88 Add AreaLights helper.

---
+++ app/plugins/built-ins/LevelScene/types/AreaLight.ts
@@ -0,0 +1,31 @@
+// @ts-ignore
+import { Object3D, RectAreaLight } from 'three';
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper';
+
+export default class AreaLight {
+  private readonly _rectLight: any;
+
+  constructor(mesh: Object3D, createHelper=false) {
+    const width = mesh.scale.x;
+    const height = mesh.scale.y;
+    const intensity = 1;
+    const rectLight = new RectAreaLight(0xffffff, intensity, width, height);
+    rectLight.position.copy(mesh.position);
+    // rectLight.lookAt(0, 0, 0);
+    rectLight.rotateX(-Math.PI / 2);
+    // rectLight.scale.set(mesh.scale);
+    this._rectLight = rectLight;
+
+    if (createHelper) {
+      const rectLightHelper = new RectAreaLightHelper(rectLight);
+      rectLight.add(rectLightHelper);
+    }
+  }
+
+  getLight(intensity: number | null = null) {
+    if (intensity !== null) {
+      this._rectLight.intensity = intensity;
+    }
+    return this._rectLight;
+  }
+}
