b260a241674beb264a8c6522f442b0c0a90e42c0
aggregate1166877 <aggregate1166877>
2023-02-07 19:24:48 +1300

    #92 Add InputType enum.

---
+++ app/plugins/built-ins/InputManager/types/InputTypes.ts
@@ -0,0 +1,14 @@
+enum InputType {
+  none = 0,
+  keyboardButton,
+  analogButton,
+  analogStickAxis,
+  mouseButton,
+  mouseAxisInfinite,
+  mouseAxisGravity,
+  mouseAxisThreshold,
+}
+
+export {
+  InputType,
+}
