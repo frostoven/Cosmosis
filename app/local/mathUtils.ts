67d050ce44e598728f016dba989da717471ea973
aggregate1166877 <aggregate1166877>
2023-02-11 19:05:56 +1300

    Add math util functions and constants.

--- app/local/mathUtils.ts
+++ app/local/mathUtils.ts
@@ -1,4 +1,8 @@
-import { Euler, Quaternion } from 'three';
+import { Euler, Quaternion, Vector3 } from 'three';
+
+const xAxis = new Vector3(1, 0, 0);
+const yAxis = new Vector3(0, 1, 0);
+const zAxis = new Vector3(0, 0, 1);
 
 const _aprEuler = new Euler(0, 0, 0, 'YXZ');
 function applyPolarRotation(x, y, observerQuaternion, minPolarAngle = 0, maxPolarAngle = Math.PI) {
@@ -9,6 +13,20 @@ function applyPolarRotation(x, y, observerQuaternion, minPolarAngle = 0, maxPola
   _aprEuler.x = y * -0.002;
   _aprEuler.x = Math.max(PI_2 - maxPolarAngle, Math.min(PI_2 - minPolarAngle, _aprEuler.x));
   observerQuaternion.setFromEuler(_aprEuler);
+}
+
+function getQuatAxis(quat: Quaternion) {
+  const radicand = 1 - (quat.w * quat.w);
+  if (radicand < Number.EPSILON) {
+    // As we approach zero, we can substitute an effective division-by-1.
+    return new Vector3(quat.x, quat.y, quat.z);
+  }
+  const squared = Math.sqrt(radicand);
+  return new Vector3(quat.x / squared, quat.y / squared, quat.z / squared);
+}
+
+function getQuatAngle(quaternion: Quaternion) {
+  return Math.acos(quaternion.w) * 2;
 }
 
 // Returns a max relative to the amount's sign. Acts like Math.max if amount
@@ -55,7 +73,12 @@ function easeIntoExp(step, max) {
 }
 
 export {
+  xAxis,
+  yAxis,
+  zAxis,
   applyPolarRotation,
+  getQuatAxis,
+  getQuatAngle,
   signRelativeMax,
   lerpToZero,
   easeIntoExp,
