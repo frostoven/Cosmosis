810b65c2bac0a6a0eaac6e60637994fd7029a996
aggregate1166877 <aggregate1166877>
2023-02-13 17:05:42 +1300

    #92 Make GamepadDriver differentiate between gamepads, HOTAS control boxes, flight sticks, and steering wheels.

--- app/plugins/built-ins/GamepadDriver/index.ts
+++ app/plugins/built-ins/GamepadDriver/index.ts
@@ -1,19 +1,75 @@
 import CosmosisPlugin from '../../types/CosmosisPlugin';
 import { gameRuntime } from '../../gameRuntime';
-import { guessGamepadName } from './types/gamepadNames';
 import { InputManager } from '../InputManager';
+import { ControllerType, guessControllerType } from './types/ControllerType';
+import { guessGamepadName } from './types/gamepadNames';
 
-const axisNames: Array<string> = [];
-const buttonNames: Array<string> = [];
+const { unknown, gamepad, hotas, flightStick, racingWheel } = ControllerType;
+
+// --- Pre-generate button names ------------------------------------------- //
+
+// In this section we pre-generate all possible button names, and then place
+// them in arrays for easy access later. This is a performance improvement; on
+// slow systems, dynamically generating strings hundreds of times a second is
+// noticeably slower under worst-case scenario conditions, and can cause
+// stutter. This method makes using customs strings for buttons extremely fast,
+// because all we end up doing is referencing values by array index.
+//
+// The ultimate purpose here is being able to easily separate incompatible
+// control bindings. For example, using an Xbox controller's bindings on a
+// HOTAS is a terrible experience, because buttons and axes of the same names
+// don't map logically (then there's the other issue that you wouldn't want a
+// flight stick and control box to share bindings).
+
+const AXIS_INDEX = 0;
+const BUTTON_INDEX = 1;
+
+const inputTypeCount = Object.values(ControllerType).length / 2;
+const inputNames: Array<Array<Array<string>>> =
+  new Array(inputTypeCount).fill(null).map(() => []);
+
+const gamepadAxisNames: Array<string> = [];
+const gamepadButtonNames: Array<string> = [];
+inputNames[gamepad][AXIS_INDEX] = gamepadAxisNames;
+inputNames[gamepad][BUTTON_INDEX] = gamepadButtonNames;
+inputNames[unknown] = inputNames[gamepad];
+//
+const hotasAxisNames: Array<string> = [];
+const hotasButtonNames: Array<string> = [];
+inputNames[hotas][AXIS_INDEX] = hotasAxisNames;
+inputNames[hotas][BUTTON_INDEX] = hotasButtonNames;
+//
+const flightStickAxisNames: Array<string> = [];
+const flightStickButtonNames: Array<string> = [];
+inputNames[flightStick][AXIS_INDEX] = flightStickAxisNames;
+inputNames[flightStick][BUTTON_INDEX] = flightStickButtonNames;
+//
+const racerStickAxisNames: Array<string> = [];
+const racerStickButtonNames: Array<string> = [];
+inputNames[racingWheel][AXIS_INDEX] = racerStickAxisNames;
+inputNames[racingWheel][BUTTON_INDEX] = racerStickButtonNames;
 
 // TODO: maybe set these to -1 or something. The first time a button is
 //  pressed, the gamepad will forcibly report a bunch of zeros and the one
 //  button that's being pressed. If -1, and button value is zero, set to zero
 //  and return. If 1, set and propagate.
+// I couldn't find any controllers that support more than 32 bindings. If this
+// turns out to be incorrect, we can increase the value.
 for (let i = 0; i < 32; i++) {
-  axisNames.push(`ax${i}`);
-  buttonNames.push(`bt${i}`);
+  gamepadAxisNames.push(`ax${i}`);
+  gamepadButtonNames.push(`bt${i}`);
+  //
+  hotasAxisNames.push(`ha${i}`);
+  hotasButtonNames.push(`hb${i}`);
+  //
+  flightStickAxisNames.push(`fa${i}`);
+  flightStickButtonNames.push(`fb${i}`);
+  //
+  racerStickAxisNames.push(`ra${i}`);
+  racerStickButtonNames.push(`rb${i}`);
 }
+
+// --- Pre-gen section end ------------------------------------------------- //
 
 // Note: this relates to how the mouse works with the game window. It has
 // nothing to do with mounting rodents, though we may or may not have such
@@ -24,8 +80,10 @@ class GamepadDriver {
   // Cached check of this.controllers. If true, controller data isn't checked
   // for updates at all.
   private _allNull: boolean;
-  private readonly _axisCache: any[][];
-  private readonly _buttonCache: any[][];
+  private readonly _axisCache: Array<Array<string>>;
+  private readonly _buttonCache: Array<Array<string>>;
+  private readonly _axisNames: Array<Array<string>>;
+  private readonly _buttonNames: Array<Array<string>>;
   private _cachedInputManager: InputManager;
 
   constructor() {
@@ -33,6 +91,8 @@ class GamepadDriver {
     this._allNull = true;
     this._axisCache = [ [], [], [], [] ];
     this._buttonCache = [ [], [], [], [] ];
+    this._axisNames = [ [], [], [], [] ];
+    this._buttonNames = [ [], [], [], [] ];
 
     window.addEventListener("gamepadconnected", this.onGamepadConnected.bind(this));
     window.addEventListener("gamepaddisconnected", this.onGamepadDisconnected.bind(this));
@@ -51,8 +111,14 @@ class GamepadDriver {
   }
 
   onGamepadConnected(event: GamepadEvent) {
-    const name = guessGamepadName(event.gamepad.id);
-    console.log(`[GamepadDriver] Connected ${name}.`);
+    const gamepad = event.gamepad;
+    const name = guessGamepadName(gamepad.id);
+    const deviceType = guessControllerType(gamepad.id);
+
+    this._axisNames[gamepad.index] = inputNames[deviceType][AXIS_INDEX];
+    this._buttonNames[gamepad.index] = inputNames[deviceType][BUTTON_INDEX];
+
+    console.log(`[GamepadDriver] Connected ${name} | treating as: ${ControllerType[deviceType]}`);
     this._allNull = false;
   }
 
@@ -76,14 +142,15 @@ class GamepadDriver {
   checkAndTriggerChanges({ index, axes, buttons }) {
     const axisCache = this._axisCache[index];
     const buttonCache = this._buttonCache[index];
-    // console.log(`-> controller ${index}: [${this._timestamps[index]}]`, buttons);
+    const axisNames = this._axisNames[index];
+    const buttonNames = this._buttonNames[index];
 
     // Propagate all axis changes.
     for (let i = 0, len = axes.length; i < len; i++) {
       const axisValue = axes[i];
       if (axisCache[i] !== axisValue) {
         axisCache[i] = axisValue;
-        // console.log(`[] axis '${axisNames[i]}' changed to`, axisValue);
+        // console.log(`[input] axis '${axisNames[i]}' changed to`, axisValue);
         this._cachedInputManager.propagateInput({ key: axisNames[i], value: axisValue });
       }
     }
@@ -93,7 +160,7 @@ class GamepadDriver {
       const buttonValue = buttons[i].value;
       if (buttonCache[i] !== buttonValue) {
         buttonCache[i] = buttonValue;
-        // console.log(`[] button ${i} changed to`, buttonValue);
+        // console.log(`[input] button ${buttonNames[i]} changed to`, buttonValue);
         // TODO: use `bt` for Xbox, Sony, and generic controllers. For others,
         //  use names that resemble the type or brand. For example, maybe name
         //  flight stick buttons `fl` and HOTAS switches `ht`. This allows
