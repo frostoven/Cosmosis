3cf3a5b8e5ac4d8a70f9089d7acc06ac29620a8c
aggregate1166877 <aggregate1166877>
2023-02-21 14:31:25 +1300

    #88 Update control schemes for ExternalLights and WarpDrive modules.

--- app/plugins/built-ins/shipModules/ExternalLights/controls.ts
+++ app/plugins/built-ins/shipModules/ExternalLights/controls.ts
@@ -1,8 +1,9 @@
 import { ActionType } from '../../InputManager/types/ActionType';
 import { ControlSchema } from '../../InputManager/interfaces/ControlSchema';
+import { InputType } from '../../InputManager/types/InputTypes';
 
 const externalLightControls: ControlSchema = {
-  cycleExternalLights: { actionType: ActionType.pulse, current: null, default: [ 'KeyL' ] },
+  cycleExternalLights: { actionType: ActionType.pulse, current: null, default: { KeyL: InputType.keyboardButton } },
 };
 
 export {
