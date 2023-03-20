d2facbb33333240fe8fe0c670d694a75abccfb2f
aggregate1166877 <aggregate1166877>
2023-03-20 17:21:43 +1300

    #101 Add debugger icons for common object.

--- app/debugger/modules/variableHijacker/components/TypeImageIcon.tsx
+++ app/debugger/modules/variableHijacker/components/TypeImageIcon.tsx
@@ -3,19 +3,28 @@ import React from 'react';
 const ICON_STYLE = {
   marginLeft: -4,
   marginBottom: -4,
-  marginRight: 4,
+  marginRight: 12,
 };
 
 const iconPath = '/css/debuggerIcons';
 
 const icon = {
+  Array: `${iconPath}/Array.png`,
   bigint: `${iconPath}/number.png`,
   boolean: `${iconPath}/boolean.png`,
+  ChangeTracker: `${iconPath}/ChangeTracker.png`,
   null: `${iconPath}/null.png`,
+  Mesh: `${iconPath}/Mesh.png`,
   number: `${iconPath}/number.png`,
   object: `${iconPath}/object.png`,
+  Object3D: `${iconPath}/Object3D.png`,
+  OrthographicCamera: `${iconPath}/aperture.png`,
+  PerspectiveCamera: `${iconPath}/aperture.png`,
+  Scene: `${iconPath}/Scene.png`,
   string: `${iconPath}/string.png`,
   undefined: `${iconPath}/undefined.png`,
+  Vector2: `${iconPath}/Vector2.png`,
+  Vector3: `${iconPath}/Vector3.png`,
 };
 
 // Further explanation:
