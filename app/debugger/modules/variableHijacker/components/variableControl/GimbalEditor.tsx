0be0f0877586e6b6ea8df6f76dcc485555cb27c3
aggregate1166877 <aggregate1166877>
2023-03-29 02:49:58 +1300

    #101 Implement GimbalEditor object rotation via mouse dragging.

--- app/debugger/modules/variableHijacker/components/variableControl/GimbalEditor.tsx
+++ app/debugger/modules/variableHijacker/components/variableControl/GimbalEditor.tsx
@@ -9,6 +9,10 @@ import Core from '../../../../../plugins/built-ins/Core';
 import PreventRender from '../../../../components/PreventRender';
 import NumberEditor from './NumberEditor';
 
+const INPUT_STYLE: React.CSSProperties = {
+  marginTop: -1,
+};
+
 const CONTAINER_STYLE: React.CSSProperties = {
   fontFamily: 'Consolas, monospace, Lato, sans-serif',
   borderLeft: '2px solid #d2d2d2',
@@ -22,6 +26,7 @@ const CONTAINER_STYLE: React.CSSProperties = {
 const CANVAS_BACKGROUND: React.CSSProperties = {
   position: 'absolute',
   background: 'url(/css/debuggerImages/background-1.jpg)',
+  border: 'thin dashed #775b77',
   backgroundSize: 'cover',
   width: 64,
   height: 64,
@@ -31,7 +36,6 @@ const CANVAS_BACKGROUND: React.CSSProperties = {
 
 const CANVAS_STYLE: React.CSSProperties = {
   position: 'absolute',
-  border: 'thin dashed #775b77',
   zIndex: 2,
 };
 
@@ -57,6 +61,11 @@ export default class GimbalEditor extends React.Component<Props> {
   private readonly renderSceneFunction: OmitThisParameter<({ delta }: { delta: any }) => void>;
   private bootAnimDone: boolean;
   private bootTimeout: number;
+  // If true, mouse button is currently down.
+  private followingMouse: boolean;
+  // What the mouse position was before the user started interacting.
+  private lastMouseX: number;
+  private lastMouseY: number;
 
   static isTypeSupported(typeName) {
     return [ 'Quaternion', 'Vector3', 'Euler' ].includes(typeName);
@@ -69,6 +78,10 @@ export default class GimbalEditor extends React.Component<Props> {
     this.renderSceneFunction = this.renderScene.bind(this);
     this.bootAnimDone = false;
     this.bootTimeout = 0.5;
+
+    this.followingMouse = false;
+    this.lastMouseX = 0;
+    this.lastMouseY = 0;
   }
 
   componentDidMount() {
@@ -85,12 +98,62 @@ export default class GimbalEditor extends React.Component<Props> {
     });
   }
 
+  setupMouseControls = () => {
+    const element: HTMLCanvasElement = this.canvasRef.current;
+
+    // This prevents a scroll hijack from happening on Windows machines.
+    element.onmousedown = function(event) {
+      if(event.button == 1) {
+        event.preventDefault();
+        return false;
+      }
+    };
+
+    // Note: we don't need to clean up persistent mouse listeners when the
+    // component is destroyed because they are automatically marked for garbage
+    // collection. Mouse-down is manually dealt with to allow for out-of-canvas
+    // mouse-up checks.
+    element.addEventListener('mousedown', (event) => {
+      event.stopPropagation();
+      this.followingMouse = true;
+      this.lastMouseX = event.screenX;
+      this.lastMouseY = event.screenY;
+
+      const mouseMouse = event => this.onMouseMove(event);
+      document.addEventListener('mousemove', mouseMouse);
+
+      const mouseUp = () => {
+        this.followingMouse = false;
+        document.removeEventListener('mouseup', mouseUp);
+        document.removeEventListener('mousemove', mouseMouse);
+      };
+
+      document.addEventListener('mouseup', mouseUp);
+    });
+  };
+
+  onMouseMove = (event) => {
+    if (this.followingMouse) {
+      const { screenX, screenY } = event;
+      const deltaX = this.lastMouseX - screenX;
+      const deltaY = this.lastMouseY - screenY;
+      this.lastMouseX = screenX;
+      this.lastMouseY = screenY;
+      const cube: THREE.Object3D = this.world3d.cube;
+
+      const group = new THREE.Group;
+      group.attach(cube);
+      group.rotateY(-deltaX * 0.07);
+      group.rotateX(-deltaY * 0.07);
+      this.world3d.scene.attach(cube);
+    }
+  };
+
   create3dWorld = () => {
     const width = 64;
     const height = 64;
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    // scene.background
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshNormalMaterial();
@@ -143,6 +206,7 @@ export default class GimbalEditor extends React.Component<Props> {
     if (allAxesReady) {
       rotation.set(0, 0, 0);
       this.bootAnimDone = true;
+      this.setupMouseControls();
     }
     else {
       const { x, y, z } = rotation;
@@ -163,9 +227,9 @@ export default class GimbalEditor extends React.Component<Props> {
     return (
       <div style={CONTAINER_STYLE}>
         <div>
-          <NumberEditor targetName={targetName} parent={parent} simplified={true}/>
-          <NumberEditor targetName={targetName} parent={parent} simplified={true}/>
-          <NumberEditor targetName={targetName} parent={parent} simplified={true}/>
+          <NumberEditor style={INPUT_STYLE} targetName={targetName} parent={parent} simplified/>
+          <NumberEditor style={INPUT_STYLE} targetName={targetName} parent={parent} simplified/>
+          <NumberEditor style={INPUT_STYLE} targetName={targetName} parent={parent} simplified/>
         </div>
         <PreventRender>
           <div style={CANVAS_FRAME}>
