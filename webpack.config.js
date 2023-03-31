06bcf359ecb1fac15067f162058a7f50a7428399
aggregate1166877 <aggregate1166877>
2023-03-31 15:05:07 +1300

    Add support for underscored variables.

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,11 @@ module.exports = {
             ],
           '@babel/preset-react'
           ],
-          plugins: [ '@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-optional-chaining' ]
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-proposal-optional-chaining',
+            '@babel/plugin-proposal-numeric-separator',
+          ]
         }
       },
       {
