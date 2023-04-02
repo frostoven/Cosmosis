afc93f826def199659581bda834e020d3cf003b6
aggregate1166877 <aggregate1166877>
2023-04-02 17:42:26 +1200

    #82 Add multiple random generator functions.

--- app/random/randomUtils.ts
+++ app/random/randomUtils.ts
@@ -1,14 +1,30 @@
+import FastDeterministicRandom from './FastDeterministicRandom';
+
+const random = Math.random;
+const ceil = Math.ceil;
+const floor = Math.floor;
+
 /**
  * This example returns a random number between the specified values. The
  * returned value is no lower than (and may possibly equal) min, and is less
  * than (and not equal) max.
  * Source:
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
- * @param min
- * @param max
+ * @param {number} min
+ * @param {number} max
  */
 function randomFloat(min, max) {
-  return Math.random() * (max - min) + min;
+  return random() * (max - min) + min;
+}
+
+/**
+ * Like randomFloat, but deterministic.
+ * @param {number} min
+ * @param {number} max
+ * @param {FastDeterministicRandom} fastRngInstance
+ */
+function randomFloatDt(min, max, fastRngInstance: FastDeterministicRandom) {
+  return fastRngInstance.next() * (max - min) + min;
 }
 
 /**
@@ -17,14 +33,27 @@ function randomFloat(min, max) {
  * isn't an integer), and is less than (but not equal to) max.
  * Source:
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
- * @param min
- * @param max
+ * @param {number} min
+ * @param {number} max
  */
 function randomIntExcl(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  min = ceil(min);
+  max = floor(max);
   // The maximum is exclusive and the minimum is inclusive
-  return Math.floor(Math.random() * (max - min) + min);
+  return floor(random() * (max - min) + min);
+}
+
+/**
+ * Like randomIntExcl, but deterministic.
+ * @param {number} min
+ * @param {number} max
+ * @param {FastDeterministicRandom} fastRngInstance
+ */
+function randomIntExclDt(min, max, fastRngInstance: FastDeterministicRandom) {
+  min = ceil(min);
+  max = floor(max);
+  // The maximum is exclusive and the minimum is inclusive
+  return floor(fastRngInstance.next() * (max - min) + min);
 }
 
 /**
@@ -34,21 +63,79 @@ function randomIntExcl(min, max) {
  * Source:
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
  * accomplishes that.
- * @param min
- * @param max
+ * @param {number} min
+ * @param {number} max
  */
 function randomInt(min, max) {
   // TODO: compare performance to crypto randomInt. If crypto is faster, create
   //  a webpack setup that dynamically uses that instead of this function when
   //  available.
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  min = ceil(min);
+  max = floor(max);
   // The maximum is inclusive and the minimum is inclusive
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return floor(random() * (max - min + 1) + min);
+}
+
+/**
+ * Like randomInt, but deterministic.
+ * @param {number} min
+ * @param {number} max
+ * @param {FastDeterministicRandom} fastRngInstance
+ */
+function randomIntDt(min, max, fastRngInstance: FastDeterministicRandom) {
+  min = ceil(min);
+  max = floor(max);
+  // The maximum is inclusive and the minimum is inclusive
+  return floor(fastRngInstance.next() * (max - min + 1) + min);
+}
+
+/**
+ * Generates a number
+ * @param {number} min
+ * @param {number} max
+ * @param {number} bias
+ * @param {number} influence
+ * @param {FastDeterministicRandom} fastRngInstance
+ */
+function randomBias(min, max, bias, influence) {
+  // Get number in range.
+  const value = randomFloat(min, max);
+  // Create influence.
+  influence *= random();
+  // Apply bias based on influence.
+  return value * (1 - influence) + bias * influence;
+}
+
+/**
+ * Like randomBias, but deterministic.
+ * @param {number} min
+ * @param {number} max
+ * @param {number} bias
+ * @param {number} influence
+ * @param {FastDeterministicRandom} fastRngInstance
+ */
+function randomBiasDt(min, max, bias, influence, fastRngInstance: FastDeterministicRandom) {
+  // Get number in range.
+  const value = randomFloatDt(min, max, fastRngInstance);
+  // Create influence.
+  influence *= fastRngInstance.next();
+  // Apply bias based on influence.
+  return value * (1 - influence) + bias * influence;
+}
+
+// Taken from Three.js, but modified to use FastDeterministicRandom.
+function randFloatSpreadDt(range, fastRngInstance: FastDeterministicRandom) {
+  return range * (0.5 - fastRngInstance.next());
 }
 
 export {
   randomFloat,
+  randomFloatDt,
   randomIntExcl,
+  randomIntExclDt,
   randomInt,
+  randomIntDt,
+  randomBias,
+  randomBiasDt,
+  randFloatSpreadDt,
 }
