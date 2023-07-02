e2a0497b2cb0914e236e37841b674253365dc70d
aggregate1166877 <noreply@frostoven.com>
2023-07-03 08:40:13 +1200

    #82 Allow star catalogs to exist as gltf files.

--- app/local/AssetFinder.js
+++ app/local/AssetFinder.js
@@ -55,7 +55,7 @@ const assetDefaults = {
   },
   starCatalogs: {
     dir: 'starCatalogs',
-    extensions: [ 'json' ],
+    extensions: [ 'json', 'glb', 'gltf' ],
   },
   voicePacks: {
     dir: 'voicePacks',
