0a7fc270f7a7c3c571878c40d1c154dc8da396f9
aggregate1166877 <noreply@frostoven.com>
2023-08-04 20:57:32 +1200

    #82 Add rotateAboutPoint function.

--- app/local/mathUtils.ts
+++ app/local/mathUtils.ts
@@ -203,6 +203,30 @@ function extractAndPopulateVerts(geo: BufferGeometry, /* distReduction=0 */) {
   return vertPositions;
 }
 
+// https://stackoverflow.com/questions/42812861/three-js-pivot-point/42866733#42866733
+// obj - your object (THREE.Object3D or derived)
+// point - the point of rotation (THREE.Vector3)
+// axis - the axis of rotation (normalized THREE.Vector3)
+// theta - radian value of rotation
+// pointIsWorld - boolean indicating the point is in world coordinates (default = false)
+function rotateAboutPoint(obj, point, axis, theta, pointIsWorld){
+  pointIsWorld = (pointIsWorld === undefined)? false : pointIsWorld;
+
+  if(pointIsWorld){
+    obj.parent.localToWorld(obj.position); // compensate for world coordinate
+  }
+
+  obj.position.sub(point); // remove the offset
+  obj.position.applyAxisAngle(axis, theta); // rotate the POSITION
+  obj.position.add(point); // re-add the offset
+
+  if(pointIsWorld){
+    obj.parent.worldToLocal(obj.position); // undo world coordinates compensation
+  }
+
+  obj.rotateOnAxis(axis, theta); // rotate the OBJECT
+}
+
 export {
   xAxis,
   yAxis,
@@ -218,4 +242,5 @@ export {
   chaseValue,
   extractVertsFromGeo,
   extractAndPopulateVerts,
+  rotateAboutPoint,
 }
