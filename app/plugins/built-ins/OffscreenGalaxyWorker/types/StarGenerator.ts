3bc14bdc1d72bad16efedcc96582adaf4fe1c9bd
aggregate1166877 <noreply@frostoven.com>
2023-08-10 20:48:30 +1200

    #82 Prepare StarGenerator for use within web worker contexts.

--- app/plugins/built-ins/OffscreenGalaxyWorker/types/StarGenerator.ts
+++ app/plugins/built-ins/OffscreenGalaxyWorker/types/StarGenerator.ts
@@ -1,9 +1,5 @@
 import * as THREE from 'three';
-import fs from 'fs';
-import { gameRuntime } from '../../../gameRuntime';
 import FastDeterministicRandom from '../../../../random/FastDeterministicRandom';
-import { galaxyDust } from '../shaders/galaxyDust.glsl';
-import AssetFinder from '../../../../local/AssetFinder';
 import Unit from '../../../../local/Unit';
 import { star } from '../shaders/star.glsl';
 import {
@@ -16,60 +12,19 @@ import {
 const unitFactor = 0.00001;
 const parsecToMLy = Unit.parsecToLy * unitFactor;
 const pi = Math.PI;
-const floor = Math.floor;
 const equatorialTilt = 23.44 * THREE.MathUtils.DEG2RAD;
 
 export default class StarGenerator {
   public rng: FastDeterministicRandom;
 
-  private _binaryCheckCache: {};
-
-  constructor({ solPosition }) {
+  constructor({ solPosition, scene, stars }) {
     this.rng = new FastDeterministicRandom();
     // Chosen with: Math.floor(Math.random() * 16384)
     this.rng.seed = 8426;
-
-    this._binaryCheckCache = {};
-
-    AssetFinder.getStarCatalog({
-      name: 'bsc5p_3d_min',
-      // name: 'constellation_test',
-      // name: 'bubble_min',
-      callback: (error, fileName, parentDir, extension) => {
-        console.log({ error, fileName, parentDir, extension });
-        if (error) {
-          return console.error(error);
-        }
-        fs.readFile(`${parentDir}/${fileName}`, (error, data) => {
-          if (error) {
-            return console.error(error);
-          }
-          let stars;
-          try {
-            // @ts-ignore
-            stars = JSON.parse(data);
-          }
-          catch (error) {
-            return console.error(error);
-          }
-
-          gameRuntime.tracked.levelScene.getOnce((scene: THREE.Scene) => {
-            this.setupStars(scene, stars, solPosition);
-          });
-        });
-      }
-    });
-  }
-
-  // Frees RAM used to check star proximity.
-  clearBinaryCache() {
-    this._binaryCheckCache = {};
+    this.setupStars(scene, stars, solPosition);
   }
 
   setupStars(scene: THREE.Scene, starObjects: any[], solPosition: THREE.Vector3) {
-    console.log('solPosition:', solPosition);
-    console.log('starPositions:', starObjects);
-
     const material = new THREE.ShaderMaterial({
       vertexShader: star.vertex,
       fragmentShader: star.fragment,
@@ -85,10 +40,7 @@ export default class StarGenerator {
 
     const visibleStars: any[] = [];
 
-    // Prevent binary star glitches by trimming out near-proximity stars. I'd
-    // have preferred combining this with the for loop after this one, but we
-    // need to have a total size before can create and loop the instanced
-    // plane.
+    // TODO: move into GalaxyDB.
     for (let i = 0, len = starObjects.length; i < len; i++) {
       const starObject = starObjects[i];
       visibleStars.push(starObject);
@@ -102,18 +54,13 @@ export default class StarGenerator {
 
     const dummy = new THREE.Object3D();
 
-    // The default catalog uses coordinates that result in an incorrect
-    // rotation in Three.js. Create a rotated version.
-    const pivotObject = new THREE.Object3D();
-    const transferObject = new THREE.Object3D();
-
     const colors: number[] = [];
     const luminosities: number[] = [];
 
     // Create instanced plane.
     for (let i = 0, len = visibleStars.length; i < len; i++) {
       const {
-        i: index, n: name, x, y, z, N: luminosity, K: color,
+        x, y, z, N: luminosity, K: color,
       } = visibleStars[i];
 
       if (color) {
@@ -139,7 +86,6 @@ export default class StarGenerator {
       dummy.updateMatrix();
       instancedPlane.setMatrixAt(i, dummy.matrix);
     }
-    this.clearBinaryCache();
 
     bufferGeometry.setAttribute('aColor', new THREE.InstancedBufferAttribute(
       new Float32Array(colors), 3,
@@ -161,7 +107,5 @@ export default class StarGenerator {
     // const material2 = new THREE.MeshBasicMaterial({ color: 0xffffff });
     // const cube = new THREE.Mesh(geometry, material2);
     // scene.add(cube);
-    window.debug.sol = solPosition;
-    window.debug.uniforms = material.uniforms;
   }
 }
