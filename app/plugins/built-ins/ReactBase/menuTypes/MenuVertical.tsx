a6909e915872b2ed75899bd65323b6bb49dc3961
aggregate1166877 <noreply@frostoven.com>
2023-10-22 23:42:06 +1300

    #108 Implement first working vertical plugin-based menu.

--- app/plugins/built-ins/ReactBase/menuTypes/MenuVertical.tsx
+++ app/plugins/built-ins/ReactBase/menuTypes/MenuVertical.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import InputBridge from '../types/InputBridge';
+import KosmButton from '../components/KosmButton';
 
 interface Props {
   options: {
-    type: string,
     default?: number,
     entries: { name: string, onSelect: Function }[],
   },
@@ -17,12 +17,15 @@ export default class MenuVertical extends React.Component<Props> {
   };
 
   state = {
-    lastAction: 'none',
     selected: null,
   };
 
   componentDidMount() {
     this._input.onAction.getEveryChange(this.handleAction);
+    const defaultIndex = this.props.options.default;
+    if (this.state.selected === null && typeof defaultIndex === 'number') {
+      this.setState({ selected: defaultIndex });
+    }
   }
 
   componentWillUnmount() {
@@ -30,25 +33,67 @@ export default class MenuVertical extends React.Component<Props> {
   }
 
   handleAction = (action: string) => {
+    const entries = this.props.options?.entries;
+    if (!entries?.length) {
+      return;
+    }
+
+    let selected = this.state.selected || 0;
+
+    if (action === 'select') {
+      const callback = entries[selected].onSelect;
+      if (typeof callback === 'function') {
+        callback({ selected, ...entries[selected] });
+      }
+      return;
+    }
+
+    let direction: number;
     if (action === 'up') {
-      console.log({ action });
-      this.setState({ lastAction: action });
+      direction = -1;
     }
     else if (action === 'down') {
-      console.log({ action });
-      this.setState({ lastAction: action });
+      direction = 1;
+    }
+    else {
+      return;
+    }
+
+    selected += direction;
+    if (selected < 0) {
+      return this.setState({ selected: 0 });
+    }
+    else if (selected >= entries.length) {
+      return this.setState({ selected: entries.length - 1 });
+    }
+    else {
+      this.setState({ selected });
     }
   };
 
   render() {
-    let selected = this.state.selected || 0;
-    if (this.state.selected === null && typeof this.props.options.default === 'number') {
-      selected = this.props.options.default;
+    const entries = this.props.options?.entries;
+    if (!entries?.length) {
+      return <div>[no menu entries available]</div>;
     }
+
+    const selected = this.state.selected || 0;
 
     return (
       <div style={this.props.style}>
-        {this.state.lastAction} {Date.now()}
+        {entries.map((menuEntry, index) => {
+          return (
+              <KosmButton
+                key={`MenuVertical-${index}`}
+                selectable
+                isActive={selected === index}
+                wide
+                block
+              >
+                {menuEntry.name}
+              </KosmButton>
+          )
+        })}
       </div>
     );
   }
