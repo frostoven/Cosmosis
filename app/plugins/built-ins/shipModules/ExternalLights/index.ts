87533a5a0d71955d1755431e49d6173466c3794c
aggregate1166877 <noreply@frostoven.com>
2023-11-06 23:24:17 +1300

    #108 Adjust externalLightControls to use InputManager control scheme storage.

--- app/plugins/built-ins/shipModules/ExternalLights/index.ts
+++ app/plugins/built-ins/shipModules/ExternalLights/index.ts
@@ -4,10 +4,19 @@ import ExternalLights from './types/ExternalLights';
 import { gameRuntime } from '../../../gameRuntime';
 import { ShipPilot } from '../../modes/playerControllers/ShipPilot';
 import { externalLightControls } from './controls';
+import { InputManager } from '../../InputManager';
 
 class ExternalLightsModule extends ModuleSpawner {
   constructor() {
     super();
+
+    InputManager.allControlSchemes.externalLightControls = {
+      key: 'externalLightControls',
+      schema: externalLightControls,
+      mergeInto: 'shipPilotControls',
+      friendly: '[merged into shipPilotControls]',
+    };
+
     gameRuntime.tracked.shipPilot.getOnce((shipPilot: ShipPilot) => {
       shipPilot.extendControlSchema(externalLightControls);
     });
