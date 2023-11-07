0db805a5fffd3f6756a5bf67ce9abc044c66ce0a
aggregate1166877 <noreply@frostoven.com>
2023-11-08 03:51:02 +1300

    #108 Add friendly name override to thrustUp10.

--- app/plugins/built-ins/modes/playerControllers/ShipPilot/controls.ts
+++ app/plugins/built-ins/modes/playerControllers/ShipPilot/controls.ts
@@ -10,7 +10,7 @@ const { keyboardButton, analogButton, analogStickAxis, mouseButton,
 
 const shipPilotControls: ControlSchema = {
   // Pulsed
-  thrustUp10:          { actionType: pulse, current: null, default: { spScrollUp: mouseButton } },
+  thrustUp10:          { actionType: pulse, current: null, default: { spScrollUp: mouseButton }, friendly: 'Thrust Increase 10%' },
   thrustReset:         { actionType: pulse, current: null, default: { spScrollDown: mouseButton } },
   mouseHeadLook:       { actionType: pulse, current: null, default: { spMouseMiddle: mouseButton, Numpad5: keyboardButton, bt11: analogButton, bt10: analogButton } },
   toggleFlightAssist:  { actionType: pulse, current: null, default: { KeyZ: keyboardButton } },
