2687cc5101de48d9bc7faa176cdbe84557074be4
aggregate1166877 <noreply@frostoven.com>
2023-11-13 03:23:46 +1300

    #108 Add additional icons to keyTypeIcons.

--- app/configs/ui.ts
+++ app/configs/ui.ts
@@ -1,7 +1,11 @@
 import { InputType } from './types/InputTypes';
+import { ScrollName } from './types/MouseButtonName';
+import { SemanticICONS } from 'semantic-ui-react';
 
-const keyTypeIcons = {
+const keyTypeIcons: { [key: string]: SemanticICONS } = {
+  // General
   default: 'dna',
+  // By input type
   [InputType.keyboardButton]: 'keyboard',
   [InputType.mouseButton]: 'mouse pointer',
   [InputType.mouseAxisInfinite]: 'crosshairs',
@@ -10,6 +14,14 @@ const keyTypeIcons = {
   [InputType.analogSlider]: 'fighter jet',
   [InputType.analogStickAxis]: 'fighter jet',
   [InputType.analogButton]: 'gamepad',
+  // By action name
+  [ScrollName.spScrollUp]: 'sort amount up',
+  [ScrollName.spScrollDown]: 'sort amount down',
+  [ScrollName.spScrollLeft]: 'text width',
+  [ScrollName.spScrollRight]: 'text width',
+  // Axes
+  MouseX: 'arrows alternate horizontal',
+  MouseY: 'arrows alternate vertical',
 };
 
 export {
