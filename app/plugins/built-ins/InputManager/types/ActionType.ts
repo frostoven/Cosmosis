ad4cc72100c30b5a60191008acc61588e58bc4c9
aggregate1166877 <noreply@frostoven.com>
2023-11-12 20:51:36 +1300

    #108 Make ActionType comments more IDE-friendly.

--- app/plugins/built-ins/InputManager/types/ActionType.ts
+++ app/plugins/built-ins/InputManager/types/ActionType.ts
@@ -1,17 +1,23 @@
 enum ActionType {
-  // Should not be used outside of debugging contexts. Does all key processing
-  // up the very last step, but then isn't handed over to the final processing
-  // functions.
+  /**
+   * Should not be used outside of debugging contexts. Does all key processing
+   * up the very last step, but then isn't handed over to the final processing
+   * functions.
+   */
   ignored = 0,
-  // This triggers a single callback per key press. Can also be thought of as a
-  // toggleable in some instances (e.g. press for roof lights, press again for
-  // no lights).
-  // This value should not change at runtime, and should be thought of as a
-  // defining factor of the action it's tied to.
+  /**
+   * This triggers a single callback per key press. Can also be thought of as a
+   * toggleable in some instances (e.g. press for roof lights, press again for
+   * no lights).
+   * This value should not change at runtime, and should be thought of as a
+   * defining factor of the action it's tied to.
+   */
   pulse = 1,
-  // If an action is continuous, it means its state is rechecked every frame.
-  // This value should not change at runtime, and should be thought of as a
-  // defining factor of the action it's tied to.
+  /**
+   * If an action is continuous, it means its state is rechecked every frame.
+   * This value should not change at runtime, and should be thought of as a
+   * defining factor of the action it's tied to.
+   */
   continuous = 2,
 }
 
