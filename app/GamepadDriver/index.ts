cdf3d68737b6d030570fa7130a03d14a233237c0
aggregate1166877 <noreply@frostoven.com>
2024-05-28 20:32:19 +1200

    Make gamepad debugging easier.

--- app/GamepadDriver/index.ts
+++ app/GamepadDriver/index.ts
@@ -5,6 +5,9 @@ const {
   unknown, gamepad, hotas, flightStick, racingWheel, pedals,
 } = ControllerType;
 
+// @ts-ignore
+window.debugGamepads = false;
+
 // --- Pre-generate button names ------------------------------------------- //
 
 // In this section we pre-generate all possible button names, and then place
@@ -177,7 +180,10 @@ export default class GamepadDriver {
       const axisValue = axes[i];
       if (axisCache[i] !== axisValue) {
         axisCache[i] = axisValue;
-        // console.log(`[input] axis '${axisNames[i]}' changed to`, axisValue);
+        // @ts-ignore
+        if (window.debugGamepads) {
+          console.log(`[input] axis '${axisNames[i]}' changed to`, axisValue);
+        }
         this.onAxisChange({ key: axisNames[i], value: axisValue });
       }
     }
@@ -187,7 +193,10 @@ export default class GamepadDriver {
       const buttonValue = buttons[i].value;
       if (buttonCache[i] !== buttonValue) {
         buttonCache[i] = buttonValue;
-        // console.log(`[input] button ${buttonNames[i]} changed to`, buttonValue);
+        // @ts-ignore
+        if (window.debugGamepads) {
+          console.log(`[input] button ${buttonNames[i]} changed to`, buttonValue);
+        }
         // TODO: use `bt` for Xbox, Sony, and generic controllers. For others,
         //  use names that resemble the type or brand. For example, maybe name
         //  flight stick buttons `fl` and HOTAS switches `ht`. This allows
