94b16992c1a280974b26c69ff8d32a476a212541
aggregate1166877 <noreply@frostoven.com>
2024-05-28 20:36:34 +1200

    Disabled formatting on control-binding files.

--- app/plugins/built-ins/shipModules/WarpDrive/controls.ts
+++ app/plugins/built-ins/shipModules/WarpDrive/controls.ts
@@ -1,3 +1,6 @@
+// @formatter:off
+// ^ Control-binding files become unreadable when formatted.
+
 import { ActionType } from '../../InputManager/types/ActionType';
 import { ControlSchema } from '../../InputManager/interfaces/ControlSchema';
 import { InputType } from '../../../../configs/types/InputTypes';
