d3270a5288d98e63d6984a278b439a61a939444d
aggregate1166877 <noreply@frostoven.com>
2024-06-17 02:14:40 +1200

    Add the Moon.

---
+++ app/plugins/built-ins/LocalSpace/SceneOverrides/scenes/Sol/EarthLuna.ts
@@ -0,0 +1,68 @@
+import * as THREE from 'three';
+import {
+  LocalStar,
+} from '../../../../../../celestialBodies/bodyTypes/LocalStar';
+import { sunMass, earthMass } from './defs';
+import { localBody } from '../../../shaders/localBody.glsl';
+import { Earth } from './Earth';
+import {
+  PlanetarySystemDefinition
+} from '../../../../../../celestialBodies/bodyTypes/PlanetarySystemDefinition';
+
+const DEG2RAD = THREE.MathUtils.DEG2RAD;
+
+class EarthLuna extends LocalStar {
+  constructor(earth: Earth) {
+    const nearMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+
+    const uniforms = {
+      objectSize: { value: 1_737_400 },
+      scale: { value: -10.0 },
+      luminosity: { value: 30000 },
+      invRadius: { value: 11.0 },
+      invGlowRadius: { value: 5.0 },
+      visibility: { value: 100 },
+      intensity: { value: 30 },
+    };
+
+    const farMaterial = new THREE.ShaderMaterial({
+      uniforms,
+      vertexShader: localBody.vertex,
+      fragmentShader: localBody.fragment,
+      // side: THREE.FrontSide,
+      side: THREE.DoubleSide,
+      blending: THREE.AdditiveBlending,
+      transparent: true,
+    });
+
+    // @ts-ignore
+    // window.planet = { uniforms };
+
+    super({
+      name: 'Luna',
+      massKg: 7.342e22,
+      parentPlanet: earth,
+      radiusM: 1_737_400,
+      rotationPeriodS: 27.32 * 24 * 3600,
+      axialTilt: 1.54 * DEG2RAD,
+      orbitalElements: {
+        semiMajorAxisM: 384400e3,
+        eccentricity: 0.0549,
+        inclination: 5.145 * DEG2RAD,
+        argPeriapsis: 318.15 * DEG2RAD,
+        ascendingNode: 125.08 * DEG2RAD,
+        meanAnomaly: 115.3654 * DEG2RAD,
+        referenceTime: 0,
+      },
+      visuals: {
+        getTexture: () => null,
+        getSphereMaterial: () => nearMaterial,
+        getDistantMaterial: () => farMaterial,
+      },
+    });
+  }
+}
+
+export {
+  EarthLuna,
+};
