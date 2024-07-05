151bcc8a0dab6b254df000a386e3aa7df0c72282
aggregate1166877 <noreply@frostoven.com>
2024-07-05 18:34:53 +1200

    Update the plugin Template to use the new plugin boilerplate.

--- app/plugins/built-ins/Template - Plugin/index.ts
+++ app/plugins/built-ins/Template - Plugin/index.ts
@@ -1,12 +1,36 @@
+import * as THREE from 'three';
 import CosmosisPlugin from '../../types/CosmosisPlugin';
+import Core from '../Core';
+import Player from '../Player';
+import PluginCacheTracker from '../../../emitters/PluginCacheTracker';
+
+// -- ✀ Plugin boilerplate ----------------------------------------------------
+
+const pluginDependencies = {
+  core: Core,
+  player: Player,
+};
+const shallowTracking = { player: { camera: 'camera' } };
+const pluginList = Object.keys(pluginDependencies);
+type Dependencies = typeof pluginDependencies & {
+  camera: THREE.Camera, // declare shallow-tracked aliases
+};
+
+// -- ✀ -----------------------------------------------------------------------
 
 class Template {
+  private _pluginCache = new PluginCacheTracker<Dependencies>(
+    pluginList, shallowTracking,
+  ).pluginCache;
+
   constructor() {
     throw 'Use this class as plugin boilerplate.';
   }
 }
 
-const templatePlugin = new CosmosisPlugin('template', Template);
+const templatePlugin = new CosmosisPlugin(
+  'template', Template, pluginDependencies,
+);
 
 export {
   Template,
