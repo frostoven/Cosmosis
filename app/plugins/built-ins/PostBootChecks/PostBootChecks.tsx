07345651bdeb67c8021a085f492fe29623dbb572
aggregate1166877 <noreply@frostoven.com>
2024-07-09 13:57:49 +1200

    Add message that makes it clear to devs when prod assets are missing.

--- app/plugins/built-ins/PostBootChecks/PostBootChecks.tsx
+++ app/plugins/built-ins/PostBootChecks/PostBootChecks.tsx
@@ -20,6 +20,39 @@ class PostBootChecks {
   private _pluginCache = new PluginCacheTracker<Dependencies>(pluginList).pluginCache;
 
   constructor() {
+    this.ensureProdAssetsExist();
+  }
+
+  ensureProdAssetsExist() {
+    fs.stat('./prodHqAssets', (error) => {
+      if (error) {
+        window.$modal.alert({
+          header: 'Assets Incomplete',
+          body: (
+            <div>
+              The high-quality assets pack is missing; you'll only see
+              basic assets, and no stars will be visible.
+              <br/><br/>
+              Please download the "Stand-Alone Production Assets" pack from
+              the Cosmosis page, which contains all HQ assets:
+              <ul>
+                <li>
+                  <a
+                    href="#"
+                    onClick={() => {
+                      // @ts-ignore - nw is valid.
+                      nw.Shell.openExternal('https://github.com/frostoven/Cosmosis/releases');
+                    }}
+                  >
+                    github.com/frostoven/Cosmosis/releases
+                  </a>
+                </li>
+              </ul>
+            </div>
+          ),
+        });
+      }
+    });
   }
 }
 
