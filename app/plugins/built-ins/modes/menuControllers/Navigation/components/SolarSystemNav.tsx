ebe69f694273659740014378b93ac0890783079a
aggregate1166877 <noreply@frostoven.com>
2024-07-23 21:00:06 +1200

    Add SolarSystemNav component.

---
+++ app/plugins/built-ins/modes/menuControllers/Navigation/components/SolarSystemNav.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SolarSystemNav extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export {
+  SolarSystemNav,
+};
