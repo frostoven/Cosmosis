ab6782bed502c920999e2b47f1bb1da419feae4e
aggregate1166877 <noreply@frostoven.com>
2024-07-23 20:59:54 +1200

    Add LocalClusterNav component.

---
+++ app/plugins/built-ins/modes/menuControllers/Navigation/components/LocalClusterNav.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class LocalClusterNav extends React.Component {
+  render() {
+    return (
+      <div>
+        System Offline.
+      </div>
+    );
+  }
+}
+
+export {
+  LocalClusterNav,
+};
