d405d18a5c304397285d6b7fe3d3fc4c8e7bb46c
aggregate1166877 <noreply@frostoven.com>
2024-07-23 21:00:14 +1200

    Add NavSettings component.

---
+++ app/plugins/built-ins/modes/menuControllers/Navigation/components/NavSettings.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class NavSettings extends React.Component {
+  render() {
+    return (
+      <div>
+        System Offline.
+      </div>
+    );
+  }
+}
+
+export {
+  NavSettings,
+};
