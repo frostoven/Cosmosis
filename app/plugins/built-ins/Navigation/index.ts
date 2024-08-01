d630903ec9100b09fde4f50df26c19393cc9b442
aggregate1166877 <noreply@frostoven.com>
2024-08-01 15:55:00 +1200

    Give the Navigation plugin the means to store and query active planetary systems.

--- app/plugins/built-ins/Navigation/index.ts
+++ app/plugins/built-ins/Navigation/index.ts
@@ -4,11 +4,18 @@ import { LocalBodies } from './interfaces/LocalBodies';
 import { LocalStars } from './interfaces/LocalStars';
 import { logBootTitleAndInfo } from '../../../local/windowLoadListener';
 import PluginLoader from '../../types/PluginLoader';
+import {
+  PlanetarySystemDefinition,
+} from '../../../celestialBodies/PlanetarySystemDefinition';
+import {
+  LargeGravitationalSource,
+} from '../../../celestialBodies/LargeGravitationalSource';
 
 // Not to be confused with the ship navigation module. This plugin offers
 // points of interest, solar system locations, and proxies universe data.
 class Navigation {
   public onNearbyStarDetected: ChangeTracker;
+  private _system: PlanetarySystemDefinition | null = null;
 
   constructor() {
     logBootTitleAndInfo('Driver', 'Navigation', PluginLoader.bootLogIndex);
@@ -21,15 +28,37 @@ class Navigation {
       spaceships: [],
       spaceStations: [],
       asteroidStations: [],
-    }
+    };
+  }
+
+  getSystemName() {
+    return this._system?.name || 'Unknown System';
   }
 
   // Returns bodies in the local solar system, if any.
   getPlanetaryData(): LocalBodies {
+    if (!this._system) {
+      return {
+        stars: [],
+        planets: [],
+        moons: [],
+      };
+    }
+
     return {
-      stars: [],
-      planets: [],
-      moons: [],
+      stars: [ this._system.mainStar ],
+      planets: this._system.planets,
+      moons: this._system.moons,
+    };
+  }
+
+  // Returns bodies in the local solar system, if any.
+  getAllPlanetaryBodies(): LargeGravitationalSource[] {
+    if (!this._system) {
+      return [];
+    }
+    else {
+      return this._system.allBodies;
     }
   }
 
@@ -38,12 +67,17 @@ class Navigation {
   getLocalStars(): LocalStars {
     return {
       stars: [],
-    }
+    };
+  }
+
+  setLocalSystem(system: PlanetarySystemDefinition) {
+    this._system = system;
   }
 }
 
 const navigationPlugin = new CosmosisPlugin('navigation', Navigation);
 
 export {
+  Navigation,
   navigationPlugin,
-}
+};
